8b9d5b4ba38e361aacf6269a5dc68a6e886f19d5
Reignable <joebarret401@gmail.com>
2021-10-17 14:12:45 +0100

    Create Nav stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,29 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: {
+    viewports: [
+      {
+        name: 'mobile',
+        styles: { width: '375px', height: '950px' },
+        type: 'mobile',
+      },
+      {
+        name: 'tablet',
+        styles: { width: '768px', height: '1024px' },
+        type: 'tablet',
+      },
+      {
+        name: 'desktop',
+        styles: { width: '1440px', height: '1024px' },
+        type: 'desktop',
+      },
+    ],
+    defaultViewport: 'mobile',
   },
 }
---
+++ src/Nav.stories.tsx
@@ -0,0 +1,34 @@
+import { Nav } from 'Nav'
+
+import { Meta } from '@storybook/react'
+import { MemoryRouter } from 'react-router'
+
+export default {
+  title: 'Nav',
+  component: Nav,
+  argTypes: {
+    planet: {
+      defaultValue: 'earth',
+      control: { type: 'select' },
+      options: [
+        'mercury',
+        'venus',
+        'earth',
+        'mars',
+        'jupiter',
+        'saturn',
+        'uranus',
+        'neptune',
+      ],
+    },
+  },
+  decorators: [
+    (Story, args) => (
+      <MemoryRouter initialEntries={[`/${args.planet}`]}>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+} as Meta
+
+export const Default = () => <Nav />
