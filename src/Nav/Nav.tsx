689f50fb856048bd69244ca37f0e527593e70ace
Reignable <joebarret401@gmail.com>
2021-10-18 10:38:11 +0100

    Finish styling mobile Nav

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { theme } from 'styles/theme'
 import { planets } from 'types'
-import { ReactComponent as HamburgerIcon } from './icon-hamburger.svg'
+import { ReactComponent as Hamburger } from './icon-hamburger.svg'
+import { ReactComponent as Chevron } from './icon-chevron.svg'
 
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
-  padding: 16px 24px 12px;
-  border-bottom: 1px solid ${theme.colors.grey};
+  align-items: center;
+  padding: 1rem 1.5rem 0.75rem;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
 `
 
 const H2 = styled.h2`
@@ -23,21 +25,53 @@ const H2 = styled.h2`
 `
 
 const IconButton = styled.button<{ menuOpen?: boolean }>`
+  padding: 0;
+  height: min-content;
   background: none;
   border: none;
-  color: ${(props) =>
-    props.menuOpen ? theme.colors.grey : theme.colors.white};
-  transition: ${theme.transitions.create(['color', 'box-shadow'])};
-  &:hover {
-    box-shadow: 0 0 0 1px currentColor;
+  color: ${props => (props.menuOpen ? theme.colors.grey : theme.colors.white)};
+  transition: ${theme.transitions.create(['color'])};
+`
+
+const List = styled.ul`
+  padding: 1.5rem;
+`
+
+const ListItem = styled.li<{ color?: string }>`
+  padding: 1.25rem 0;
+  font-family: 'Spartan', sans-serif;
+  font-weight: 700;
+  line-height: 25px;
+  font-size: 15px;
+  letter-spacing: 1.36364px;
+  text-transform: uppercase;
+  color: ${theme.colors.white};
+  display: grid;
+  grid-template-columns: max-content 1fr max-content;
+  column-gap: 1.5rem;
+  align-items: center;
+
+  & + & {
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
   }
+
+  & svg {
+    margin-right: 0.5rem;
+  }
+`
+
+const ColorCircle = styled.div<{ color?: string }>`
+  width: 20px;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  background-color: ${props => props.color};
 `
 
 const Nav = (): JSX.Element => {
   const [menuOpen, setMenuOpen] = useState(false)
 
   const handleMenuButtonClick = () => {
-    setMenuOpen((c) => !c)
+    setMenuOpen(c => !c)
   }
 
   return (
@@ -49,20 +83,25 @@ const Nav = (): JSX.Element => {
           onClick={handleMenuButtonClick}
           menuOpen={menuOpen}
         >
-          <HamburgerIcon />
+          <Hamburger />
         </IconButton>
       </Header>
       {menuOpen && (
         <nav>
-          <ul>
-            {planets.map((planet) => (
-              <li>
-                <Link key={planet} to={`/${planet}/overview`}>
-                  {planet.toUpperCase()}
-                </Link>
-              </li>
+          <List>
+            {planets.map(planet => (
+              <Link
+                component={ListItem}
+                role="link"
+                to={`/${planet}/overview`}
+                key={planet}
+              >
+                <ColorCircle color={theme.colors[planet]} />
+                {planet.toUpperCase()}
+                <Chevron />
+              </Link>
             ))}
-          </ul>
+          </List>
         </nav>
       )}
     </>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,13 +2,21 @@ const theme = {
   colors: {
     grey: '#979797',
     white: '#fff',
+    mercury: '#def4fc',
+    venus: '#f7cc7f',
+    earth: '#545bfe',
+    mars: '#ff6a45',
+    jupiter: '#ecad7a',
+    saturn: '#fccb6b',
+    uranus: '#65f0d5',
+    neptune: '#497efa',
   },
   transitions: {
     duration: '150ms',
     easing: 'ease-in-out',
     create(properties: string[]): string {
       return properties
-        .map((property) => `${property} ${this.duration} ${this.easing}`)
+        .map(property => `${property} ${this.duration} ${this.easing}`)
         .join(`,`)
     },
   },
