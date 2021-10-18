d86bcfc66de1898a5cc153bee98f703a9cd9d0d5
Reignable <joebarret401@gmail.com>
2021-10-18 11:03:50 +0100

    Change media queries

--- src/styles/media-queries.ts
+++ src/styles/media-queries.ts
@@ -2,20 +2,19 @@ import { useMediaQuery } from 'react-responsive'
 
 type Sizes = 'mobile' | 'tablet' | 'desktop'
 
-const breakpoints = [
-  { name: 'mobile', size: 375 },
-  { name: 'tablet', size: 768 },
-  { name: 'desktop', size: 1440 },
-]
+const breakpoints: { [key in Sizes]: number } = {
+  mobile: 375,
+  tablet: 768,
+  desktop: 1440,
+}
 
-const mq = breakpoints.reduce<{
-  [key in Sizes]: string
-}>(
-  (mediaQueries, bp) => ({
-    ...mediaQueries,
-    [bp.name]: `@media (min-width: ${bp.size}px)`,
-  }),
-  { mobile: '', tablet: '', desktop: '' },
-)
+const mq = {
+  up(breakpoint: Sizes): string {
+    return `(min-width: ${breakpoints[breakpoint]}px)`
+  },
+  down(breakpoint: Sizes): string {
+    return `(max-width: ${breakpoints[breakpoint]}px)`
+  },
+}
 
 export { mq, useMediaQuery }
